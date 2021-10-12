e71d8b1fd68418e2fb18e6e10e1703b934f0f107
Batget <alexey667@mail.ru>
2021-10-12 21:37:53 +0300

    feat(InnovationPak) start project

---
+++ src/main/java/by/belohvostik/InnovationPak/InnovationPakApplication.java
@@ -0,0 +1,13 @@
+package by.belohvostik.InnovationPak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovationPakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovationPakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/belohvostik/InnovationPak/InnovationPakApplicationTests.java
@@ -0,0 +1,13 @@
+package by.belohvostik.InnovationPak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovationPakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
